091e9b66e10f32b68c52fd7e75fc4281fad2fd13
SP Waduge <141314329+spwaduge@users.noreply.github.com>
2024-12-13 13:03:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new Typed('#role', {
+        strings: ['Web Developer', 'Computer Technician', 'Tech Enthusiast'],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    });
+});
+
